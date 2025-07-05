cada403d7f1b81c161b4abf7e5c9152a286efe78
reddy442 <reddyroyal442@gmail.com>
2025-07-05 12:36:46 +0530

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -25,8 +25,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html'],
-    ['list'],
-    ['allure-playwright', { outputFolder: 'my-allure-results' }]
+    ['list']
   ],  
   ///npx playwright test --reporter='allure-palywright',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
