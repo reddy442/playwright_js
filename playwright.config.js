bd869322b8733655cd239566c0c6e081b009505f
reddy442 <reddyroyal442@gmail.com>
2025-07-05 13:07:05 +0530

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -24,8 +24,8 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['html'],
-    ['list']
+     ['list'],
+    ['html', { outputFolder: 'playwright-report', open: 'never' }]
   ],  
   ///npx playwright test --reporter='allure-palywright',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
